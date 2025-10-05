e39f9ba5040e2f1d1f75081f887b4787a34e949f
CIMON <spacechunksstudio@gmail.com>
2025-10-05 11:15:23 +0200

    chore: add Minecraft version(s) 1.21.9

--- notify-bungeecord/build.gradle.kts
+++ notify-bungeecord/build.gradle.kts
@@ -18,6 +18,7 @@ modrinth {
     versionType.set("beta")
     uploadFile.set(tasks.shadowJar)
     gameVersions.addAll(
+        
         "1.20",
         "1.20.1",
         "1.20.2",
@@ -34,6 +35,8 @@ modrinth {
         "1.21.6",
         "1.21.7",
         "1.21.8",
+        "1.21.9",
+
     )
     loaders.add("bungeecord")
     loaders.add("waterfall")
