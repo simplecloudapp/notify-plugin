0346fe8198aef61f4ba579defd569632be2faac3
Jannis <0limitdev@gmail.com>
2026-04-28 16:58:56 +0200

    deps

--- notify-shared/build.gradle.kts
+++ notify-shared/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     api(libs.bundles.configurate)
+    api(libs.simplecloud.plugin)
     compileOnly(libs.simplecloud.api)
     compileOnly(libs.adventure.api)
     compileOnly(libs.adventure.text.minimessage)
